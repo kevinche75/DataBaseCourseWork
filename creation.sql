5c3cc8a8f37ff75e623016e34f6d6e8e3a6359dd
Елизавета <liza@tune-it.ru>
2020-10-14 19:05:27 +0300

    пофиксила некоторые айди, но не все. айди полета, сидения и букинга все еще сериал(может быть не только они, но именно их ацди учавствуют во внешних ключах)

--- creation.sql
+++ creation.sql
@@ -1,4 +1,14 @@
 create type comp_type as enum ('airport', 'cafe', 'cleaning', 'airline');
+create type flight_status as enum (
+  'scheduled', 'delayed',
+  'departed', 'in air',
+  'expected', 'diverted',
+  'recovery', 'landed',
+  'arrived', 'cancelled',
+  'no takeoff info', 'past flight');
+create type baggage_status as enum ('lost','accept','sent','returned');
+create type seat_class as enum('business','economy');
+create type room_class as enum('middle', 'comfort','comfort+');
 
 create table company (
   name varchar(30) primary key,
@@ -6,11 +16,12 @@ create table company (
 );
 
 create table employee (
-  id serial primary key,
+  passport_no char(10) primary key,
   company varchar(30) references company (name) on delete cascade not null,
   name varchar(30) not null,
   second_name varchar(30) not null,
   third_name varchar(30),
+
   position varchar(20) not null
 );
 
@@ -21,25 +32,18 @@ create table aircraft (
   model varchar(30) not null
 );
 
-create type flight_status as enum (
-  'scheduled', 'delayed',
-  'departed', 'in air',
-  'expected', 'diverted',
-  'recovery', 'landed',
-  'arrived', 'cancelled',
-  'no takeoff info', 'past flight');
 
 create table flight (
   id serial primary key,
   aircraft_id varchar(10) references aircraft(id) on delete cascade,
-  schedule_departure timestamp not null,
-  schedule_arrival timestamp not null,
-  actual_departure timestamp not null,
-  actual_arrival timestamp not null,
+  schedule_departure timestamptz not null,
+  schedule_arrival timestamptz not null,
+  actual_departure timestamptz not null,
+  actual_arrival timestamptz not null,
   status flight_status not null,
   departure_airport varchar(4) not null,
   arrival_airport varchar(4) not null,
-  check ( departure_airport <> arrival_airport )
+  check ( departure_airport <> arrival_airport and schedule_arrival>schedule_departure and actual_arrival>actual_departure)
 );
 
 CREATE OR REPLACE FUNCTION flight_insert() RETURNS trigger AS '
@@ -55,51 +59,48 @@ EACH ROW EXECUTE PROCEDURE flight_insert();
 
 create table reception_schedule (
   id serial primary key,
-  employee_id int references employee(id) on delete set null,
+  employee_id char(10) references employee(passport_no) on delete set null,
   flight_id int references flight(id) on delete cascade,
   reception_number smallint not null,
-  start_time timestamp not null,
-  finish_time timestamp not null,
+  start_time timestamptz not null,
+  finish_time timestamptz not null,
   check ( reception_number > 0 )
 );
 
 create table gate_schedule (
   id serial primary key,
-  employee_id int references employee(id) on delete set null,
+  employee_id char(10) references employee(passport_no) on delete set null,
   flight_id int references flight(id) on delete cascade,
   gate_number smallint not null,
-  start_time timestamp not null,
-  finish_time timestamp not null,
+  start_time timestamptz not null,
+  finish_time timestamptz not null,
   check ( gate_number > 0 )
 );
 
 create table crew (
-  employee_id int references employee(id) on delete set null,
+  employee_id char(10) references employee(passport_no) on delete set null,
   flight_id int references flight(id) on delete cascade,
   primary key (employee_id,
                flight_id)
 );
 
-create type baggage_status as enum ('lost','accept','sent','returned');
-create type seat_class as enum('business','economy');
-create type room_class as enum('middle', 'comfort','comfort+');
+
 create table passenger(
-  id serial primary key,
+  passport_no char(10) primary key ,
   name varchar(30) not null,
   second_name varchar(30) not null,
   third_name varchar(30),
-  passport_no char(10) not null,
   birthday date not null,
-  check (birthday<now())
+  check (birthday<current_date)
 );
 create table booking(
   id serial primary key,
-  book_date timestamp not null,
+  book_date timestamptz not null,
   total_amount integer,
   time_amount integer,
   contact_data text,
   check(total_amount>0 and time_amount>0),
-  check(book_date<now())
+  check(book_date<current_date)
 );
 create table seat(
   id serial primary key,
@@ -110,14 +111,14 @@ create table seat(
 );
 create table ticket(
   id serial primary key,
-  passanger_id integer not null,
+  passanger_id char(10),
   flight_id integer not null,
   seat_id integer not null,
   amount integer not null,
   book_id integer not null,
   registered boolean not null,
   foreign key(book_id) references booking(id) on delete cascade,
-  foreign key(passanger_id) references passenger(id),
+  foreign key(passanger_id) references passenger(passport_no),
   foreign key (flight_id) references flight(id),
   foreign key (seat_id) references seat(id),
   check(amount>0)
@@ -141,15 +142,26 @@ create table relax_room_booking(
 );
 
 insert into company(name,type) values ('S7','airline');
-insert into employee(company, name, second_name, third_name, position) VALUES ('S7','ivanov','ivan','ivamnovi4','pilot');
+insert into employee(passport_no,company, name, second_name, third_name, position) VALUES ('1111111111','S7','ivanov','ivan','ivamnovi4','pilot');
 insert into aircraft (id,location, owner_id, model) values ('a111','Russia','S7','boeing');
 insert into flight(aircraft_id, schedule_departure, schedule_arrival,actual_departure,actual_arrival, status, departure_airport, arrival_airport)
 values ('a111','1999-01-08 04:05:06','1999-01-08 07:05:06','1999-01-08 04:05:06','1999-01-08 07:05:06','arrived','svo','led');
 insert into reception_schedule(employee_id, flight_id, reception_number, start_time, finish_time)
-VALUES (1,2,1,'1999-01-08 02:05:06','1999-01-08 03:05:06');
+VALUES ('1111111111',1,18787,'1999-01-08 02:05:06','1999-01-08 03:05:06');
 insert into gate_schedule(employee_id, flight_id, gate_number, start_time, finish_time)
-VALUES (1,2,2,'1999-01-08 03:05:06','1999-01-08 04:05:06') ;
-insert into crew(employee_id, flight_id) VALUES (1,2);
+VALUES ('1111111111',1,1889,'1999-01-08 03:05:06','1999-01-08 04:05:06') ;
+insert into crew(employee_id, flight_id) VALUES ('1111111111',1);
+insert into passenger(passport_no,name, second_name, third_name, birthday)
+VALUES ('3333333333','petrov','petr','petrovi4','180-01-08');
+insert into booking(book_date, total_amount, time_amount, contact_data)
+values ('1999-01-02 03:05:06',6,6,'676787');
+insert into seat(number, flight_id, class) values ('21a',1,'economy');
+insert into ticket(passanger_id, flight_id, seat_id, amount, book_id, registered)
+values ('3333333333',1,1,12,1,true );
+insert into baggage(ticket_id, total_weight, max_weight, status)
+values (1,3,3,'lost');
+insert into relax_room_booking(ticket_id, class)
+values (1,'comfort+');
 
 
 
